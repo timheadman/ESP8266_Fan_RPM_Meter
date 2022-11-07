17e229600e730894dc51426880687c1e27e630a0
tim <tim@omarov.net>
2022-11-07 20:45:42 +0300

    add PWM

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,24 @@ void setup()
   attachInterrupt(0, rpm, CHANGE);
   lcd.init();
   lcd.backlight();
+  analogWrite(9, 1);
 }
 
 void loop()
 {
+  static uint8_t i = 0;
   val = 0;    // сбрасываем счетчик и ждем.
-  delay(500); // так как прерывание CHANGE, срабатывает два раза
-              // то и считываем каждые пол секунды.
+  delay(500); // так как прерывание CHANGE, срабатывает два раза, считываем каждые пол секунды
+  if (i > 55) i = 0;
+  analogWrite(9, i++);
   lcd.home();
-   lcd.print((val * 60) / 2); // количество импульсов на 60 секунд
-                                // и делим на количество импульсов на оборот
-  lcd.print(" rpm ");
-  lcd.setCursor(0, 1);
+  lcd.print((val * 60) / 2); // количество импульсов на 60 секунд и делим на количество импульсов на оборот
+  lcd.print("rpm; ");
   lcd.print(val);
-  lcd.print(" val");
+  lcd.print("val             ");
+  lcd.setCursor(0, 1);
+  lcd.print(i);
+  lcd.print("                ");
+
+
 }
