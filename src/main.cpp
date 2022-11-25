71906c8b5072a038136b236fac2806ed2bdcf6d8
Tim <tim@omarov.net>
2022-11-25 20:49:37 +0300

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
-#include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
-volatile int val;
+#include "main.h"
+
+volatile uint16_t val;
+volatile uint8_t button;
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-void rpm()
+void rpm_interrupt()
 {
   val++;
+}
+
+void button_interrupt()
+{
+  button += 5;
 }
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println(F("\nInitializing... "));
-  attachInterrupt(0, rpm, CHANGE);
+  attachInterrupt(1, rpm_interrupt, CHANGE);
+  attachInterrupt(0, button_interrupt, FALLING);
   lcd.init();
   lcd.backlight();
   analogWrite(9, 1);
@@ -20,19 +27,18 @@ void setup()
 
 void loop()
 {
-  static uint8_t i = 0;
   val = 0;    // сбрасываем счетчик и ждем.
   delay(500); // так как прерывание CHANGE, срабатывает два раза, считываем каждые пол секунды
-  if (i > 55) i = 0;
-  analogWrite(9, i++);
+  button += 5;
+  analogWrite(9, button);
   lcd.home();
+  lcd.print("RPM:");
   lcd.print((val * 60) / 2); // количество импульсов на 60 секунд и делим на количество импульсов на оборот
-  lcd.print("rpm; ");
+  lcd.print(" (");
   lcd.print(val);
-  lcd.print("val             ");
+  lcd.print(") - " + String(String((val * 60) / 2).length()));
+  lcd.print(val);
   lcd.setCursor(0, 1);
-  lcd.print(i);
+  lcd.print(button);
   lcd.print("                ");
-
-
 }
